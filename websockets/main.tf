6566778a1444b09823858e378e34d1edd4e31f79
amdingo <dingop83@gmail.com>
2018-03-15 02:23:51 -0400

    updates for ec2 data

---
+++ websockets/main.tf
@@ -0,0 +1,51 @@
+# Specify the provider and access details
+provider "aws" {
+  region = "${var.aws_region}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+}
+
+data "terraform_remote_state" "backbone" {
+  backend = "atlas"
+  config {
+    name         = "AlphaStack/backbone"
+    access_token = "2JCkLM3YbJMXnw.atlasv1.HqlNxgwQMB7HcuJsKoNiSNsGGJZc8phkvZpizyEhrqJioMLlNySBbsBlLVtBAyvuqos"
+  }
+}
+
+data "aws_lb" "lb" {
+  id = "${var.alb_id}"
+}
+
+resource "aws_instance" "websocket_server" {
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+  connection {
+    # The default username for our AMI
+    user = "alphastack"
+
+    # The connection will use the local SSH agent for authentication.
+  }
+
+  instance_type = "${var.instance_type}"
+
+  # Lookup the correct AMI based on the region
+  # we specified
+  # ami = "${lookup(var.aws_amis, var.aws_region)}"
+  ami = "${var.ws_ami}"
+
+  # The name of our SSH keypair we created above.
+  key_name = "${var.key_pair_id}"
+
+  # Our Security group to allow SSH access
+  vpc_security_group_ids = ["${var.security_group_id}"]
+
+  # Launches the instance into the default subnet
+  subnet_id = "${var.subnet_id}"
+
+  # Name it in the tags
+  tags {
+    Name        = "AlphaStack Production Bastion Server"
+    AppVersion  = "Beta"
+  }
+}
---
+++ websockets/variables.tf
@@ -0,0 +1,30 @@
+variable "public_key" {}
+
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "aws_region" {
+  description = "same as parent provider"
+}
+
+variable "alb_id" {
+  description = "application load balancer that will forward traffic"
+}
+
+variable "instance_type" {
+  description = "Type of instance to launch, must be minimum of 2 vcpu"
+  default = "t2.medium"
+}
+
+variable "ws_ami" {
+  description = "The current websocket AMI"
+}
+
+variable "key_pair_id" {}
+
+variable "subnet_id" {}
+
+variable "security_group_id" {
+  description = "private security group id"
+}
